d8e3bf98ee3bc414019284f8ee155f2c8516ddba
April Granzow <apriljgranzow@gmail.com>
2021-11-17 13:58:35 -0800

    Added "not null" constraints and some default values

--- create.sql
+++ create.sql
@@ -3,11 +3,11 @@ CREATE TABLE questions (
   id SERIAL UNIQUE PRIMARY KEY,
   product_id INTEGER not null,
   body character varying(1000),
-  date_written bigint,
-  asker character varying(60),
-  email text,
-  reported boolean,
-  helpful INTEGER
+  date_written bigint not null,
+  asker character varying(60) not null,
+  email text not null,
+  reported boolean not null DEFAULT FALSE,
+  helpful INTEGER not null DEFAULT 0
 );
 
 COPY questions
@@ -25,12 +25,12 @@ SET
 CREATE TABLE answers (
   id SERIAL UNIQUE PRIMARY KEY,
   question_id INTEGER references questions(id),
-  body character varying(1000),
+  body character varying(1000) not null,
   date_written bigint,
-  answerer character varying(60),
-  email text,
-  reported boolean,
-  helpful integer
+  answerer character varying(60) not null,
+  email text not null,
+  reported boolean DEFAULT FALSE,
+  helpful integer DEFAULT 0
 );
 
 COPY answers
@@ -47,7 +47,7 @@ SET
 CREATE TABLE answers_photos (
   id SERIAL UNIQUE PRIMARY KEY,
   answer_id INTEGER references answers(id),
-  url text
+  url text not null
 );
 
 COPY answers_photos FROM 'answers_photos.csv' DELIMITER ',' CSV HEADER;
