2389990aad803fb8bcb65be78cbe370239c51fad
April Granzow <apriljgranzow@gmail.com>
2021-11-18 14:06:55 -0800

    Added answers route, changed queries to return result in response

--- server/app.js
+++ server/app.js
@@ -15,17 +15,37 @@ app.get('/qa/questions', (req, res) => {
   const page = req.query.page === undefined ? 1 : req.query.page;
   const count = req.query.count === undefined ? 5 : req.query.count;
   // OFFSET says to skip that many rows before beginning to return rows.
-  const offset = page * count;
+  const offset = (page-1) * count;
   const text = `
     SELECT * FROM questions
       WHERE product_id = $1
       AND reported is FALSE
-      LIMIT $2`;
-  db.query(text, [product_id, count]).then((result) => {
-    console.log(result.rows);
+      LIMIT $2
+      OFFSET $3`;
+  db.query(text, [product_id, count, offset]).then((result) => {
+    res.json(result.rows);
   })
     .catch((e) => console.error(e.stack));
-  res.end();
+});
+
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  const { question_id } = req.params;
+  const page = req.query.page === undefined ? 1 : req.query.page;
+  const count = req.query.count === undefined ? 5 : req.query.count;
+  const offset = (page - 1) * count;
+  const text = `
+    SELECT * FROM answers
+      WHERE question_id = $1
+      AND reported IS FALSE
+      LIMIT $2
+      OFFSET $3
+  `;
+  db.query(text, [question_id, count, offset]).then(
+    (result) => {
+      res.json(result.rows);
+    },
+  )
+    .catch((e) => console.error(e.stack));
 });
 
 app.listen(port, () => {
