64921fcd49c019ea487eba1a1b03ccb4a96a8c5e
April Granzow <apriljgranzow@gmail.com>
2021-11-20 10:10:06 -0800

    Added default timestamps to questions and answers tables

--- create.sql
+++ create.sql
@@ -9,18 +9,11 @@ CREATE TABLE questions (
   reported boolean not null DEFAULT FALSE,
   helpful INTEGER not null DEFAULT 0
 );
-
 COPY questions
-from
-  'questions.csv' DELIMITER ',' CSV HEADER;
-
-ALTER TABLE
-  questions
-ALTER COLUMN
-  date_written
-SET
-  DATA TYPE timestamp with time zone USING timestamp with time zone 'epoch' + date_written * interval '1 millisecond';
-
+from 'questions.csv' DELIMITER ',' CSV HEADER;
+ALTER TABLE questions
+ALTER COLUMN date_written
+SET DATA TYPE timestamp with time zone USING timestamp with time zone 'epoch' + date_written * interval '1 millisecond';
 --id,question_id,body,date_written,answerer_name,answerer_email,reported,helpful
 CREATE TABLE answers (
   id SERIAL UNIQUE PRIMARY KEY,
@@ -32,22 +25,21 @@ CREATE TABLE answers (
   reported boolean DEFAULT FALSE,
   helpful integer DEFAULT 0
 );
-
 COPY answers
-from
-  'answers.csv' DELIMITER ',' CSV HEADER;
-
-ALTER TABLE
-  answers
-ALTER COLUMN
-  date_written
-SET
-  DATA TYPE timestamp with time zone USING timestamp with time zone 'epoch' + date_written * interval '1 millisecond';
-
+from 'answers.csv' DELIMITER ',' CSV HEADER;
+ALTER TABLE answers
+ALTER COLUMN date_written
+SET DATA TYPE timestamp with time zone USING timestamp with time zone 'epoch' + date_written * interval '1 millisecond';
 CREATE TABLE answers_photos (
   id SERIAL UNIQUE PRIMARY KEY,
   answer_id INTEGER references answers(id),
   url text not null
 );
-
-COPY answers_photos FROM 'answers_photos.csv' DELIMITER ',' CSV HEADER;
+COPY answers_photos
+FROM 'answers_photos.csv' DELIMITER ',' CSV HEADER;
+ALTER TABLE questions
+ALTER COLUMN date_written
+SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE answers
+ALTER COLUMN date_written
+SET DEFAULT CURRENT_TIMESTAMP;
