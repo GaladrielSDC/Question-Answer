9efcead9cf3e7d926e5efe13ef6a1c7252062734
April Granzow <apriljgranzow@gmail.com>
2021-11-30 09:48:14 -0800

    Unit test for Get Answers Endpoint + dependencies for testing

--- spec/qa.test.js
+++ spec/qa.test.js
@@ -5,7 +5,22 @@ const url = 'http://localhost:3000/';
 describe('Questions', () => {
   test('has a get questions endpoint', async () => {
     await pactum.spec()
-      .get(`${url}qa/questions`)
-      .expectStatus(200);
+      .get(`${url}qa/questions?product_id=6270`)
+      .expectStatus(200)
+      .expectJsonSchemaAt('product_id', { type: 'string' })
+      .expectJsonSchemaAt('results', { type: 'array' });
+  });
+});
+
+describe('Answers', () => {
+  test('has a get answers endpoint', async () => {
+    await pactum.spec()
+      .get(`${url}qa/questions/22091/answers`)
+      .expectStatus(200)
+      .expectBodyContains('question')
+      .expectBodyContains('page')
+      .expectBodyContains('count')
+      .expectBodyContains('results')
+      .expectJsonSchemaAt('results', { type: 'array' });
   });
 });
