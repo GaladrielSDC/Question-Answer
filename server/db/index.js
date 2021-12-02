60b547e73299e9aef12a589c26af128795fa78a6
April Granzow <apriljgranzow@gmail.com>
2021-12-02 09:57:43 -0800

    Refactored database connection to use environment variables

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,9 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: '',
-  host: 'localhost',
-  database: 'qa',
-  password: '',
-  port: 5432,
-});
-
+const pool = new Pool();
 
 module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  }
-}
+  query: (text, params, callback) => (
+    pool.query(text, params, callback)
+  ),
+};
